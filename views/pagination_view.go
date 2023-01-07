3f7be68bb7cf9b848d125c116e54b4de9f187f80
karintou8710 <karintou8710@gmail.com>
2023-01-07 23:05:52 +0900

    [fix] delete sort column of view

--- views/pagination_view.go
+++ views/pagination_view.go
@@ -3,7 +3,6 @@ package views
 type PaginationView struct {
 	Limit      int         `json:"limit"`
 	Page       int         `json:"page"`
-	Sort       string      `json:"sort"`
 	TotalRows  int64       `json:"totalRows"`
 	TotalPages int         `json:"totalPages"`
 	Rows       interface{} `json:"rows"`
